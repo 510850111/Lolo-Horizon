96ffd47dad08f2e5bbbdea7f2d7008941de23d29
510850111 <510850111@qq.com>
2017-06-29 13:22:47 +0800

    新增游戏入口文件

---
+++ src/runGame/RunGame.js
@@ -0,0 +1,23 @@
+//游戏主文件,入口文件
+(function () {
+    /**
+     * 游戏入口
+     */
+    function RunGame() {
+        RunGame.__super.call(this);
+        this.init();
+    }
+
+    //RunGame 是一个显示对象 继承此 Sprite 注册这个RunGame类
+    Laya.class(RunGame, "RunGame", laya.display.Sprite);
+
+    //定义RumGame的原型
+    var _proto = RunGame.prototype;
+
+    //初始化
+    _proto.init = function () {
+        console.log("RunGame进行初始化");
+        
+    }
+
+})();
