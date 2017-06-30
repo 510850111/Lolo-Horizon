13e92ba217b5578137dae249ed5ded501bb801b6
510850111 <510850111@qq.com>
2017-06-30 13:58:58 +0800

    更新到地板的显示与滚动

--- src/runGame/Floor.js
+++ src/runGame/Floor.js
@@ -1,27 +1,78 @@
-(function(){
+//定义背景的大小
+var BG_WIDTH = 852;
+var BG_HEIGHT = 450;
+//地板高度
+var FLOOR_HEIGHT = 84;
+var FLOOR_WIDTH = 960;
+
+(function () {
     /**
      * 地板类
      */
-    function Floor(){
+    function Floor() {
+
+        //背景贴图
+        this.bgTexture = null;
+        //背景
+        this.bg = null;
+
         Floor.__super.call(this);
     }
 
     //注册这个类
-    Laya.class(Floor,"Floor",laya.display.Sprite);
+    Laya.class(Floor, "Floor", laya.display.Sprite);
 
     var _proto = Floor.prototype;
-    _proto.init = function(type){
+
+    _proto.init = function (type) {
+        /**
+         * 初始化Floor
+         */
+        console.log("Floor.init");
+        //如果不开启autoSize 父容器的宽度和高度无法获取
+        this.autoSize = true;
+        //初始化的时候将坐标放到屏幕右边
+        this.x = BG_WIDTH;
+        //应该把地板放在屏幕中间
+        this.y = (BG_HEIGHT - FLOOR_HEIGHT) / 2;
+
+        if (this.bg == null) {
+            //贴图纹理
+            this.bgTexture = Laya.loader.getRes("res/floor.png");
+            this.bg = new laya.display.Sprite();
+            //清空绘制
+            this.bg.graphics.clear();
+
+            this.addChild(this.bg);
+        }
+        //绘制地板
+        this.bg.graphics.drawTexture(this.bgTexture, 0, 0)
         //创建一个帧循环处理函数
-        Laya.timer.frameLoop(1,this,this.onLoop);
+        Laya.timer.frameLoop(1, this, this.onLoop);
     }
 
-    _proto.onLoop = function(){
+    _proto.onLoop = function () {
         /**
          * 帧循环处理函数
          */
+
+        //让地板的速度和移动比背景快一点
+        this.x -= 3.2;
+
+        if (( this.x + BG_WIDTH + FLOOR_WIDTH) < 0) {
+            //判断整个floor是否不在屏幕里面了 如果不在了 移除当前floor
+            Laya.timer.clear(this, this.onLoop);
+
+            this.visible = false;
+
+            this.removeSelf();
+            
+            console.log("Floor已被清除");
+        }
+
     }
 
-    _proto.addItem = function(){
+    _proto.addItem = function () {
         /**
          * 允许在地板上添加物品
          */
--- src/runGame/MapFloor.js
+++ src/runGame/MapFloor.js
@@ -1,41 +1,52 @@
-(function(){
+(function () {
     /**
      * 地板地图管理类
      */
-    function MapFloor(){
+    function MapFloor() {
+
         MapFloor.__super.call(this);
+        
         this.init();
     }
 
     //注册MapFloor
-    Laya.class(MapFloor,"MapFloor",laya.display.Sprite);
+    Laya.class(MapFloor, "MapFloor", laya.display.Sprite);
 
     var _proto = MapFloor.prototype;
-    _proto.init = function(){
+
+    _proto.init = function () {
+
+        this.addFloor();
         //创建一个帧循环函数
-        Laya.timer.frameLoop(1,this,this.onLoop);
+        Laya.timer.frameLoop(1, this, this.onLoop);
 
     }
 
-    _proto.onLoop = function(){
+    _proto.onLoop = function () {
         /**
          * 帧循环处理
          */
     }
 
-    _proto.addFloor = function(){
+    _proto.addFloor = function () {
         /**
          * 增加地板
          */
+
+        var floor = new Floor();
+
+        floor.init();
+
+        this.addChild(floor);
     }
 
-    _proto.getFloor = function(){
+    _proto.getFloor = function () {
         /**
          * 获取地板
          */
     }
 
-    _proto.delFloor = function(){
+    _proto.delFloor = function () {
         /**
          * 删除地板
          */
